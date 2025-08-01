a77f63c4cf338974bffa39d46b46c502e61f9857
klu-2200031578 <2200031578@kluniversity.in>
2025-08-01 20:45:19 +0530

    sixth commit

--- Difference.java
+++ Difference.java
@@ -2,6 +2,10 @@ public class Difference {
     public static void main(String[] args) {
         int a = 20, b = 10;
         int tot = a - b;
+        int tot1=a+b;
+        System.out.println("Add: "+tot1);
         System.out.println("Diff: " + tot);
+
+        System.out.flush();
     }
 }
